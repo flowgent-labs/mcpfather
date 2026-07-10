1863c2a91feb69042d91d5c59fc3efc2542dcde3
James Wong <jameswong1376@gmail.com>
2026-07-11 02:17:14 +0800

    fix: Updated the README generate

--- examples/jira-mcp/mcpclient.sh
+++ examples/jira-mcp/mcpclient.sh
@@ -67,7 +67,7 @@ EOEX
 EOEX
   cat <<'EOEX'
   # AddComment (POST)
-  ./mcpclient.sh call AddComment '{"expand": "expand_value", "issueIdOrKey": "issueIdOrKey_value", "body": {"id": "10000", "updateAuthor": {}, "properties": [], "visibility": {}, "author": "value", "updated": "2012-07-06T18:30:00.000+0000", "created": "2012-07-06T18:30:00.000+0000", "renderedBody": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget venenatis elit. Duis eu justo eget augue iaculis fermentum. Sed semper quam laoreet nisi egestas at posuere augue semper.", "self": "http://www.example.com/jira/rest/api/2/issue/10010/comment/10000", "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget venenatis elit. Duis eu justo eget augue iaculis fermentum. Sed semper quam laoreet nisi egestas at posuere augue semper."}}'
+  ./mcpclient.sh call AddComment '{"expand": "expand_value", "issueIdOrKey": "issueIdOrKey_value", "body": {"updateAuthor": {}, "updated": "2012-07-06T18:30:00.000+0000", "properties": [], "self": "http://www.example.com/jira/rest/api/2/issue/10010/comment/10000", "visibility": {}, "author": "value", "renderedBody": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget venenatis elit. Duis eu justo eget augue iaculis fermentum. Sed semper quam laoreet nisi egestas at posuere augue semper.", "created": "2012-07-06T18:30:00.000+0000", "id": "10000", "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget venenatis elit. Duis eu justo eget augue iaculis fermentum. Sed semper quam laoreet nisi egestas at posuere augue semper."}}'
 EOEX
   cat <<'EOEX'
   # AddField (POST)
@@ -87,11 +87,11 @@ EOEX
 EOEX
   cat <<'EOEX'
   # AddSharePermission (POST)
-  ./mcpclient.sh call AddSharePermission '{"id": "id_value", "body": {"userKey": "userKey_value", "view": false, "edit": false, "groupname": "groupname_value", "projectId": "projectId_value", "projectRoleId": "projectRoleId_value", "type": "type_value"}}'
+  ./mcpclient.sh call AddSharePermission '{"id": "id_value", "body": {"view": false, "edit": false, "groupname": "groupname_value", "projectId": "projectId_value", "projectRoleId": "projectRoleId_value", "type": "type_value", "userKey": "userKey_value"}}'
 EOEX
   cat <<'EOEX'
   # AddTab (POST)
-  ./mcpclient.sh call AddTab '{"screenId": 0, "body": {"id": 10000, "name": "Fields Tab"}}'
+  ./mcpclient.sh call AddTab '{"screenId": 0, "body": {"name": "Fields Tab", "id": 10000}}'
 EOEX
   cat <<'EOEX'
   # AddUserToApplication1 (POST)
@@ -111,7 +111,7 @@ EOEX
 EOEX
   cat <<'EOEX'
   # AddWorklog (POST)
-  ./mcpclient.sh call AddWorklog '{"adjustEstimate": "adjustEstimate_value", "issueIdOrKey": "issueIdOrKey_value", "newEstimate": "newEstimate_value", "reduceBy": "reduceBy_value", "body": {"timeSpentSeconds": 12000, "comment": "I did some work here.", "created": "2010-07-14T18:23:23.733+0000", "self": "http://www.example.com/jira/rest/api/2/issue/10010/worklog/10000", "started": "2010-07-14T18:23:23.733+0000", "updated": "2010-07-14T18:23:23.733+0000", "author": "value", "id": "100028", "issueId": "10002", "updateAuthor": {}, "visibility": {}, "timeSpent": "3h 20m"}}'
+  ./mcpclient.sh call AddWorklog '{"adjustEstimate": "adjustEstimate_value", "issueIdOrKey": "issueIdOrKey_value", "newEstimate": "newEstimate_value", "reduceBy": "reduceBy_value", "body": {"issueId": "10002", "visibility": {}, "self": "http://www.example.com/jira/rest/api/2/issue/10010/worklog/10000", "timeSpentSeconds": 12000, "started": "2010-07-14T18:23:23.733+0000", "comment": "I did some work here.", "updated": "2010-07-14T18:23:23.733+0000", "created": "2010-07-14T18:23:23.733+0000", "author": "value", "timeSpent": "3h 20m", "updateAuthor": {}, "id": "100028"}}'
 EOEX
   cat <<'EOEX'
   # ApplyEmailTemplates (POST)
